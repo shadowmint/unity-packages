66c8f31229cdf150fd3ee15ae5dc17a5b0feda11
doug <linderd@iinet.net.au>
2015-11-04 20:59:11 +0800

    Adds thing

--- package-assets/Factory.cs
+++ package-assets/Factory.cs
@@ -20,20 +20,36 @@ namespace N.Package.Assets {
     }
 
     /// Create a new material with the given shader and texture
-    public bool Material(string texture, string shader, string output, string textureId = "_MainTex") {
-      var sp = Shader.Find(shader);
-      var tp = Resources.Load<Texture>(texture);
-      var asset = new Material(sp);
-      asset.SetTexture(textureId, tp);
-      AssetDatabase.CreateAsset(asset, Path.Combine(path, output).ToString());
-      return true;
+    public Option<Material> Material(string texture, string shader, string output, string textureId = "_MainTex") {
+      try {
+        var sp = Shader.Find(shader);
+        var tp = Resources.Load<Texture>(texture);
+        var asset = new Material(sp);
+        asset.SetTexture(textureId, tp);
+        asset.SetFloat("_Mode", 3.0f);
+        AssetDatabase.CreateAsset(asset, Path.Combine(path, output).ToString());
+        return Option.Some(asset);
+      }
+      catch(Exception err) {
+        N.Console.Error(err);
+        return Option.None<Material>();
+      }
+    }
+
+    /// Create a quad game object in the current scene
+    public Option<GameObject> Quad(Material material) {
+      return Option.None<GameObject>();
     }
   }
 
   public class FactoryTests : N.Tests.TestSuite {
+
     public void test_material_factory() {
       var factory = new Factory(Packages.Relative("package-assets/Resources/package-assets/tests").Unwrap());
-      factory.Material("package-assets/tests/test.png", "Specular", "test.mat");
+      Assert(factory.Material("package-assets/tests/test", "Standard", "test.mat").IsSome);
+    }
+
+    public void test_quad_factory() {
     }
   }
 }
